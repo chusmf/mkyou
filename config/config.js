520fa1205e77f85d5eab7a74d3ae279b3c9eed3b
chusmf <chusmf@gmail.com>
2014-04-11 14:09:45 +0200

    config

---
+++ config/config.js
@@ -0,0 +1,118 @@
+enlaces_old = {
+'twitter':'Twitter',
+'Facebook':'Facebook',
+'App_android':'Aplicacion android',
+'App_ios':'Aplicacion IOS',
+'linkedin':'Linkedin',
+}
+
+enlaces = [['Twitter','twitter'],['Facebook','facebook'],['App android','app_android'],['App IOS','app_ios'],['Linkedin','linkedin']]
+
+filtros = ['Edad','Sexo','Localizacion','Pareja','Dinero','Estudios']
+
+
+parejas = {
+'soltero_a':'Soltero/a',
+'tengo_pareja':'Tengo pareja',
+'casado_a':'Casado/a',
+'divorciado_a':'Divorciado/a',
+'viudo_a':'Viudo/a',
+}
+
+
+
+comunidades = {
+//'0':'Seleccionar una comunidad',
+'andalucia':'Andalucía',
+'aragon':'Aragón',
+'asturias':'Asturias',
+'baleares':'Baleares',
+'canarias':'Canarias',
+'cantabria':'Cantabria',
+'castilla_mancha':'Castilla la Mancha',
+'castilla_leon':'Castilla y León',
+'cataluna':'Cataluña',
+'comunidad_valenciana':'Comunidad Valenciana',
+'extremadura':'Extremadura',
+'galicia':'Galicia'
+}
+
+
+
+andalucia = {
+'sevilla' : 'Sevilla',
+'huelva' : 'Huelva',
+'cordoba' : 'Córdoba',
+'almeria' : 'Almería',
+'jaen' : 'Jaén',
+'cadiz' : 'Cádiz',
+'granada' : 'Granada',
+'malaga' : 'Málaga'
+}
+
+aragon = {
+'zaragoza' : 'Zaragoza',
+'huesca' : 'Huesca',
+'teruel' : 'Teruel'
+}
+
+asturias = {
+'asturias' : 'Asturias',
+}
+
+baleares = {
+'baleares' : 'Baleares',
+}
+
+canarias = {
+'canarias' : 'Canarias',
+}
+
+cantabria = {
+'cantabria' : 'Cantabria',
+}
+
+castilla_mancha = {
+'toledo' : 'Toledo',
+'cuenca' : 'Cuenca',
+'guadalajara' : 'Guadalajara',
+'cuenca' : 'Cuenca',
+'ciudad_real' : 'Ciudad Real',
+}
+
+castilla_leon = {
+'zamora' : 'Zamora',
+'leon' : 'León',
+'valladolid' : 'Valladolid',
+'palencia' : 'Palencia',
+'burgos' : 'Burgos',
+'soria' : 'Soria',
+'salamanca' : 'Salamanca',
+'segovia' : 'Segovia',
+'avila' : 'Ávila',
+}
+
+cataluna = {
+'barcelona' : 'Barcelona',
+'tarragona' : 'Tarragona',
+'lleida' : 'Lleida',
+'girona' : 'Girona',
+}
+
+comunidad_valenciana = {
+'valencia' : 'Valencia',
+'castellon' : 'Castellón',
+'alicante' : 'Alicante',
+}
+
+extremadura = {
+'caceres' : 'Cáceres',
+'badajoz' : 'Badajoz',
+}
+
+galicia = {
+'a_coruña' : 'A Coruña',
+'lugo' : 'Lugo',
+'orense' : 'Orense',
+'pontevedra' : 'Pontevedra',
+}
---
+++ config/config_reveal.js
@@ -0,0 +1,36 @@
+fondos_imagen = {
+'Azul luz':'http://www.abogae.com/mkyou/encuesta/img/fondos/a1.jpg',
+'Verde luz':'http://www.abogae.com/mkyou/encuesta/img/fondos/a2.jpg',
+'Violeta luz':'http://www.abogae.com/mkyou/encuesta/img/fondos/a3.jpg',
+'Morado luz':'http://www.abogae.com/mkyou/encuesta/img/fondos/a4.jpg',
+'Rojo luz':'http://www.abogae.com/mkyou/encuesta/img/fondos/a5.jpg',
+'Amarillo':'http://www.abogae.com/mkyou/encuesta/img/fondos/1.jpg',
+'Rosa':'http://www.abogae.com/mkyou/encuesta/img/fondos/2.jpg',
+'Morado':'http://www.abogae.com/mkyou/encuesta/img/fondos/3.jpg',
+'Azul':'http://www.abogae.com/mkyou/encuesta/img/fondos/4.jpg',
+'Verde':'http://www.abogae.com/mkyou/encuesta/img/fondos/5.jpg',
+'Membrete':'http://www.abogae.com/mkyou/encuesta/img/fondos/large.jpg',
+'Gris':'http://www.abogae.com/mkyou/encuesta/img/fondos/squairy_light.png',
+}
+
+//fuentes = ['Tangerine','Verdana','Arial','Courier','Droid Sans','Roboto Condensed','Open Sans']
+fuentes = {'tangerine':'Tangerine','verdana':'Verdana','arial':'Arial','courier':'Courier','droid_sans':'Droid Sans','roboto_condensed':'Roboto Condensed','open_sans':'Open Sans'}
+fuentes_usar = ['Tangerine','Verdana','Arial','Courier','Droid Sans','Roboto Condensed','Open Sans']
+
+//efectos = ['default','cube','page','concave','zoom','linear','fade','none']
+efectos = {'default':'Linear','cube':'Cubo','page':'Pagina','concave':'Concavo','zoom':'Zoom','fade':'Degradado','none':'Ninguno'}
+
+velocidad = {'default':'Medio','slow':'Lento','fast':'Rapido'}
+
+
+/*fondos_imagen = {
+'sky':'-webkit-radial-gradient(center, circle cover, #f7fbfc 0%, #add9e4 100%)',
+'beige':'-webkit-radial-gradient(center, circle cover, white 0%, #f7f2d3 100%)',
+'blood':'-webkit-radial-gradient(center, circle cover, #626262 0%, #f00 100%)',
+'simple':'#fafafa',
+'solarized':'#fdf6e3',
+'serif':'-webkit-radial-gradient(center, circle cover, #999 0%, #ff6699 100%)',
+'night':'#111111',
+'moon':'#002b36',
+'default':'-webkit-radial-gradient(center, circle cover, #555a5f 0%, #1c1e20 100%)',
+}*/
